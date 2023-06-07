d3fadc2809798cfe94778c7d765bd8bea8647d34
mantejohannes <mokibelomante@gmail.com>
2023-06-07 10:35:33 +0200

    first commit

---
+++ website/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
+
+
+class Navbar extends Component{
+
+    render(){
+        return(
+
+            <nav className = "NavbarItems"> 
+         
+            <h1 className="navbar-logo">Mante</h1>
+
+            <ul className="nav-menu">
+           
+            {MenuItems.map((item,index) =>{
+                return(
+                        
+                <li key= {index}>
+                <a className = {item.cName} href="/">
+                <i className={item.icon}></i>
+                {item.title}
+                </a>
+                </li>
+
+                )
+            })}
+
+
+            </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
---
+++ website/src/routes/About.js
@@ -0,0 +1,13 @@
+function About (){
+    return(
+        <>
+            <h1>This is About</h1>
+        </>
+
+
+    )
+
+
+}
+
+export default About;
